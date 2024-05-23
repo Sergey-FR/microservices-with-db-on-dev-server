0d5cf9b8ec38c2d5c3a64cdee12ddc8f1cfd480c
Sergey FR <54309596+Sergey-FR@users.noreply.github.com>
2024-05-23 11:57:34 +0300

    Update development-server-userdata.sh

--- terraform-file/development-server-userdata.sh
+++ terraform-file/development-server-userdata.sh
@@ -10,6 +10,7 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compo
 chmod +x /usr/local/bin/docker-compose
 yum install git -y
 yum install java-11-amazon-corretto -y
-git clone https://github.com/cmakkaya/microservices-with-db-on-dev-server.git
+git clone https://github.com/Sergey-FR/microservices-with-db-on-dev-server.git
 cd microservices-with-db-on-dev-server
+git branch dev
 git checkout dev
